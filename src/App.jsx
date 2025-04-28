84126cffbb88abb07a54b01a0e325c24c2f26c4c
max gromov <maximgromov@gmail.com>
2025-04-28 11:38:26 +0300

    add fetch to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  //const [count, setCount] = useState(0)
+  //https://jsonplaceholder.typicode.com/photos
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  constructor(props){
+    super(props);
+    this.state = {
+      photos: []
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(res => res.json())
+    .then(photos =>{
+      this.setState({
+        photos
+      });
+    });
+  }
+
+  render() {
+    return (
+    <div>
+      <ul>
+        {this.state.photos.map((photo,index)=>{
+          return <li key={index}>{photo.title}</li>;
+        })}
+      </ul>
+    </div>
+    );
+  }
 }
 
-export default App
+export default App;
